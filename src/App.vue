654f3f35a1fce240e8e7c88ec4bc4348761e57f3
Shuai Pang <shuai.pang@innocellence.com>
2023-06-21 13:56:40 +0800

    svg使用

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// 引入图标组件
+import { Plus, Edit } from '@element-plus/icons-vue'
+console.log(import.meta.env)
+
+</script>
 
 <template>
   <div>后台管理系统</div>
+  <svg style="width:100px">
+    <!-- xlink:href执行用哪一个图标，属性值务必#icon-图标名字 -->
+    <!-- usebiaoqianfill属性可以设置图标颜色 -->
+    <use xlink:href="#icon-phone" fill="red"></use>
+  </svg>
+  
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
+
