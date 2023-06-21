eaa050fd7d87de6dee0e682f3909d4bb96fca4f5
Shuai Pang <shuai.pang@innocellence.com>
2023-06-21 14:19:44 +0800

    封装svg

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 // 引入图标组件
 import { Plus, Edit } from '@element-plus/icons-vue'
+import SvgIcon from '@/components/svg-icon/index.vue'
 console.log(import.meta.env)
 </script>
 
 <template>
   <div>后台管理系统</div>
-  <svg style="width: 100px">
-    <!-- xlink:href执行用哪一个图标，属性值务必#icon-图标名字 -->
-    <!-- usebiaoqianfill属性可以设置图标颜色 -->
-    <use xlink:href="#icon-phone" fill="red"></use>
-  </svg>
+<svg-icon name="heart" color="red" width="100px" height="100px"></svg-icon>
 </template>
 
 <style scoped></style>
---
+++ src/components/svg-icon/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <svg :style="{width, height}">
+    <!-- xlink:href执行用哪一个图标，属性值务必#icon-图标名字 -->
+    <!-- usebiaoqianfill属性可以设置图标颜色 -->
+    <use :xlink:href="prefix + name" :fill="color"></use>
+  </svg>
+</template>
+
+<script setup lang="ts">
+    defineProps({
+        // xlink:href属性前缀
+        prefix: {
+            type: String,
+            default: '#icon-'
+        },
+        // 提供使用图标名字
+        name: String,
+        // 接受父组件传递颜色
+        color: {
+            type: String,
+            default: 'blue'
+        },
+        // 接受父组件传递宽度
+        width: {
+            type: String,
+            default: '16px'
+        },
+        // 接受父组件传递高度
+        height: {
+            type: String,
+            default: '16px'
+        }
+    })
+</script>
+
+<style scoped>
+</style>
