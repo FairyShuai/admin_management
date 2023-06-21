c1eba12aa5b1fac584693dcd20e824a355cd5c0c
Shuai Pang <shuai.pang@innocellence.com>
2023-06-21 18:16:11 +0800

    登录路由静态配置

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 // 引入element-plus插件与样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
 // @ts-ignore
 // 配置element-plus国际化
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
-
-// 获取应用实例对象
-const app = createApp(App)
-
-// 安装element-plus插件
-app.use(ElementPlus, {
-  locale: zhCn, //国际化
-})
-
+// 引入模版的全局样式
+import '@/styles/index.scss'
 // svg插件需要配置代码
 import 'virtual:svg-icons-register'
 // 引入自定义插件对象： 注册整个项目全局组件
 import GlobalComponent from '@/components'
+import router from './router'
+
+// 获取应用实例对象
+const app = createApp(App)
+// 安装element-plus插件
+app.use(ElementPlus, {
+  locale: zhCn, //国际化
+})
 // 安装自定义插件
 app.use(GlobalComponent)
-
-// 引入模版的全局样式
-import '@/styles/index.scss'
-
-// 测试代码：测试假接口能否好用
-import axios from 'axios'
-
+// 注册模板路由
+app.use(router)
 // 将应用挂在到挂载点上
 app.mount('#app')
---
+++ src/views/home/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>我以为我可以撸完20节课，实际进度只有10。。。</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
