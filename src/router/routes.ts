471f097bf3053147b959f693691e7eb40c55a056
Shuai Pang <shuai.pang@innocellence.com>
2023-06-28 17:11:09 +0800

    品牌管理静态table

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -94,6 +94,54 @@ export const ConstantRoute = [
       },
     ],
   },
+  {
+    path: '/product',
+    component: () => import('@/layout/index.vue'),
+    name: 'Product',
+    meta: {
+      title: '商品管理',
+      icon: 'Goods',
+    },
+    redirect: '/product/trademark',
+    children: [
+      {
+        path: '/product/trademark',
+        component: () => import('@/views/product/trademark/index.vue'),
+        name: 'Trademark',
+        meta: {
+          title: '品牌管理',
+          icon: 'ShoppingCartFull',
+        },
+      },
+      {
+        path: '/product/attr',
+        component: () => import('@/views/product/attr/index.vue'),
+        name: 'Attr',
+        meta: {
+          title: '属性管理',
+          icon: 'ChromeFilled',
+        },
+      },
+      {
+        path: '/product/spu',
+        component: () => import('@/views/product/spu/index.vue'),
+        name: 'Spu',
+        meta: {
+          title: 'SPU管理',
+          icon: 'Calendar',
+        },
+      },
+      {
+        path: '/product/sku',
+        component: () => import('@/views/product/sku/index.vue'),
+        name: 'Sku',
+        meta: {
+          title: 'SKU管理',
+          icon: 'Orange',
+        },
+      },
+    ],
+  },
 ]
 
 //异步路由
@@ -137,54 +185,54 @@ export const asnycRoute = [
       },
     ],
   },
-  // {
-  //   path: '/product',
-  //   component: () => import('@/layout/index.vue'),
-  //   name: 'Product',
-  //   meta: {
-  //     title: '商品管理',
-  //     icon: 'Goods',
-  //   },
-  //   redirect: '/product/trademark',
-  //   children: [
-  //     {
-  //       path: '/product/trademark',
-  //       component: () => import('@/views/product/trademark/index.vue'),
-  //       name: 'Trademark',
-  //       meta: {
-  //         title: '品牌管理',
-  //         icon: 'ShoppingCartFull',
-  //       },
-  //     },
-  //     {
-  //       path: '/product/attr',
-  //       component: () => import('@/views/product/attr/index.vue'),
-  //       name: 'Attr',
-  //       meta: {
-  //         title: '属性管理',
-  //         icon: 'ChromeFilled',
-  //       },
-  //     },
-  //     {
-  //       path: '/product/spu',
-  //       component: () => import('@/views/product/spu/index.vue'),
-  //       name: 'Spu',
-  //       meta: {
-  //         title: 'SPU管理',
-  //         icon: 'Calendar',
-  //       },
-  //     },
-  //     {
-  //       path: '/product/sku',
-  //       component: () => import('@/views/product/sku/index.vue'),
-  //       name: 'Sku',
-  //       meta: {
-  //         title: 'SKU管理',
-  //         icon: 'Orange',
-  //       },
-  //     },
-  //   ],
-  // },
+  {
+    path: '/product',
+    component: () => import('@/layout/index.vue'),
+    name: 'Product',
+    meta: {
+      title: '商品管理',
+      icon: 'Goods',
+    },
+    redirect: '/product/trademark',
+    children: [
+      {
+        path: '/product/trademark',
+        component: () => import('@/views/product/trademark/index.vue'),
+        name: 'Trademark',
+        meta: {
+          title: '品牌管理',
+          icon: 'ShoppingCartFull',
+        },
+      },
+      {
+        path: '/product/attr',
+        component: () => import('@/views/product/attr/index.vue'),
+        name: 'Attr',
+        meta: {
+          title: '属性管理',
+          icon: 'ChromeFilled',
+        },
+      },
+      {
+        path: '/product/spu',
+        component: () => import('@/views/product/spu/index.vue'),
+        name: 'Spu',
+        meta: {
+          title: 'SPU管理',
+          icon: 'Calendar',
+        },
+      },
+      {
+        path: '/product/sku',
+        component: () => import('@/views/product/sku/index.vue'),
+        name: 'Sku',
+        meta: {
+          title: 'SKU管理',
+          icon: 'Orange',
+        },
+      },
+    ],
+  },
 ]
 
 //任意路由
